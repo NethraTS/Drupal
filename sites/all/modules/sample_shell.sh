014a2242ced659a2fe66d1d8cace5290aa16990e
Nethra <nethra.suthagar@terralogic.com>
2017-03-23 16:01:49 +0000

    Change 2

--- sites/all/modules/sample_shell.sh
+++ sites/all/modules/sample_shell.sh
@@ -1,15 +1,2 @@
-<?php
-/**
- *  HumanClass class file
- *  Php version 5.2
- *
- *  @category Human_Family_A
- *  @package  Human_Family_A
- *  @author   purencool <purencool@gmail.com>
- *  @license  http://opensource.org/licenses/GPL-3.0 GNU Public License
- *  @link     http://purencool.com
- *
- */
-
-?>
-
+#!/bin/bash
+echo "Hello !!"
